7c326b2072b8b517fbd8dddfccdb8fe68b17c1e6
raiz <rayees417@gmail.com>
2022-09-28 00:35:40 +0000

    ratio and factorial

--- utilities/ratioFactorial/index.js
+++ utilities/ratioFactorial/index.js
@@ -1,12 +1,10 @@
-const factNum = require("./factorial");
-const rationNums = require("./ratio");
+const factNum = require("../factorial");
+const rationNums = require("../ratio");
 
-const ratioAndFactorial = function (num1, num2, num3) {
+const ratioAndFactorial = (num1, num2, num3) => {
   const ratio = ratioOfTwoNumbers(num1, num2);
-  const factNum = factorialOfNumber(num3);
-  const output = {
-    ratio: ratio,
-    factorial: factNum,
-  };
-  console.log(output);
+  const factorial = factorialOfNumber(num3);
+
+  return { ratio, factorial };
 };
+module.exports = ratioAndFactorial;
